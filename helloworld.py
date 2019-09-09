42baf519caed0c16879ee1f00fcfd4caadf5cbae
aadhar <aadhar@umich.edu>
2019-09-09 12:21:31 -0400

    helloworld

---
+++ helloworld.py
@@ -0,0 +1,10 @@
+def main():
+    print("Aadhar Agarwal")
+    print(23)
+    print(45)
+    print("Sum = {}".format(23 + 45))
+    print("Average = {}".format((23 + 45)/2))
+
+
+if __name__ == '__main__':
+    main()
